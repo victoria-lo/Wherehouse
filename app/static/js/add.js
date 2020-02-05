6393081456c9e293874127fbac3229ecb7a53061
VicLo <victoria2666@gmail.com>
2020-02-04 21:19:50 -0500

    added functions

--- app/static/js/add.js
+++ app/static/js/add.js
@@ -1,12 +1,22 @@
+"use strict";
+
+function init(e){
+    const addItem = document.querySelector('#addNewItem');
+    addItem.addEventListener('click', addInventory);
+}
+
+
 //Adds inventory the itemData and push item to warehouseData
-function addInventory(){
+function addInventory(e){
+    e.preventDefault();
+	const itemName = document.getElementById("itemName").value;
+	const itemQty = document.getElementById("itemQty").value;
+	const itemWarehouse = document.getElementById("warehouse").value;
 
-	var itemName = document.getElementById("itemName").value;
-	var itemQty = document.getElementById("itemQty").value;
-	var itemWarehouse = document.getElementById("warehouse").value;
-
-	var newItem = new Item(itemName, itemQty, itemWarehouse);
-	itemData.push(newItem);
+	//const newItem = new Item(itemName, itemQty, itemWarehouse);
+	//itemData.push(newItem);
 	console.log("Added "+ itemQty+ " "+ itemName +" into warehouse " + itemWarehouse);
 
 }
+
+document.addEventListener('DOMContentLoaded', init);
--- app/static/js/inventory.js
+++ app/static/js/inventory.js
@@ -1,3 +1,5 @@
+"use strict";
+
 //initialize objects and global variables
 let numberOfItems = 0;
 let numberOfWarehouses = 0; 
@@ -16,24 +18,22 @@ class Item {
     }
 }
 //Warehouse
-class Warehouse {
-	constructor(capacity) {
-        this.items = [];
-		this.capacity = capacity; //total capacity
-		
-		//storage = sum of all item quantity, init as 0
-		this.storage = 0;
+function Warehouse(capacity) {
+	this.items = [];
+	this.capacity = capacity; //total capacity
+	
+	//storage = sum of all item quantity, init as 0
+	this.storage = 0;
 
-		this.warehouseId = numberOfWarehouses;
-		numberOfWarehouses++;
-    }
+	this.warehouseId = numberOfWarehouses;
+	numberOfWarehouses++;
 }
 
 //calculates the total storage used in the warehouse
 Warehouse.prototype = {
 	getStorage: function(){
 		for(let i = 0; i < this.items.length; i++){
-            this.storage += this.items[i].quantity;
+			this.storage += this.items[i].quantity;
 		}
 		return this.storage;
 	}
@@ -41,16 +41,20 @@ Warehouse.prototype = {
 //To get the storage of a warehouse, warehouseData[0].getStorage()
 
 //initial data item[0]
-if(itemData.length < 1){
-	itemData.push(new Item('Ice Cream', 65, 0));
+function init(e){
+	if(itemData.length < 1){
+		itemData.push(new Item('Ice Cream', 65, 0));
+	
+		warehouseData.push(new Warehouse(450)); //warehouse 0
+		warehouseData.push(new Warehouse(650)); //warehouse 1
+		warehouseData.push(new Warehouse(500)); //warehouse 2
+		warehouseData.push(new Warehouse(250)); //warehouse 3
+		warehouseData.push(new Warehouse(1000)); //warehouse 4
+	
+		warehouseData[0].items.push(itemData[0]); // push item[0] to items list in warehouse 0
+	}
 
-	warehouseData.push(new Warehouse(450)); //warehouse 0
-	warehouseData.push(new Warehouse(650)); //warehouse 1
-	warehouseData.push(new Warehouse(500)); //warehouse 2
-	warehouseData.push(new Warehouse(250)); //warehouse 3
-	warehouseData.push(new Warehouse(1000)); //warehouse 4
-
-	warehouseData[0].items.push(itemData[0]); // push item[0] to items list in warehouse 0
 }
+
 
 
